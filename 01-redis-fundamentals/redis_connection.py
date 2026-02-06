9fa98f755f9f8015a7a2244ad8bd3001a3a6853d
Jamezfn <jamesmakachia19@gmail.com>
2026-02-06 15:31:54 +0300

    Connecting to redis

---
+++ 01-redis-fundamentals/redis_connection.py
@@ -0,0 +1,11 @@
+import asyncio
+import redis.asyncio as redis
+
+async def main():
+    client = redis.Redis()
+    print(f"Ping successful: {await client.ping()}")
+
+    await client.aclose()
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ 01-redis-fundamentals/redis_page_views.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+import redis.asyncio as redis
+
+app = FastAPI()
+
